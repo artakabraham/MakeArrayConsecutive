b206ed1e71941ff77ebd90e3161a527e26b89bef
Artak Abrahamyan <abrahamyan@outlook.com>
2021-05-13 10:42:27 +0400

    Add Comment

--- MakeArrayConsecutive/Program.cs
+++ MakeArrayConsecutive/Program.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 
 namespace MakeArrayConsecutive
-{
+{    
     class Program
     {
+        /// <summary>
+        /// Ratiorg got statues of different sizes as a present from CodeMaster for his birthday, each statue having an non-negative integer size. 
+        /// Since he likes to make things perfect, he wants to arrange them from smallest to largest so that each statue will be bigger than the 
+        /// previous one exactly by 1. He may need some additional statues to be able to accomplish that. Help him figure out the minimum number 
+        /// of additional statues needed.
+        /// </summary>
         static void Main(string[] args)
-        {
+        {           
+            
             int[] statues = new int[] { 3, 0 };
             int t;
 
@@ -29,7 +36,6 @@ namespace MakeArrayConsecutive
                     tmp += statues[i + 1] - statues[i] - 1;
                 }
             }
-
             Console.WriteLine(tmp);
         }
     }
